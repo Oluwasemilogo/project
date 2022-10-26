8a7e4845ba0ac1accbd93d535a874f06ef3bc802
Semilogo Bamidele <emmanbamidele471@gmail.com>
2022-10-26 01:16:48 +0100

    fetched the github api data.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const api = await fetch("https://api.github.com/users/Oluwasemilogo/repos");
+    const data = await api.json();
+    setUsers(data);
+  };
+  console.log(users);
   return (
     <div className="App">
       <h1>HELLO WORLD.</h1>
