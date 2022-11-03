fb56585daa836d63d089ee9d307f5d4ca821da0c
Semilogo Bamidele <emmanbamidele471@gmail.com>
2022-11-03 16:15:19 +0100

    fixed some errors.

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, {  useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 // import { DetailsContext } from "./Context";
 import "../App.css";
